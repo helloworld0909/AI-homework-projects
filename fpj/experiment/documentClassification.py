4f1ca97fabd8a58aabc13beeecf7a7d7ea6821b3
Zheng Luo <helloworld09@163.com>
2017-06-26 23:31:05 +0800

    document classification linear model

--- fpj/experiment/documentClassification.py
+++ fpj/experiment/documentClassification.py
@@ -14,7 +14,7 @@ from fpj.lda.lda import LDA
 
 menu_path = cwd + '/input/reuters-21578/'
 
-n_topic=20
+n_topic=60
 n_iter=100
 
 def fit(corpus):
@@ -27,17 +27,37 @@ def fit(corpus):
     model.save_model(filepath='save/reuters-21578/', protocol=2)
 
 def linear_classifier_bench(corpus):
-    from sklearn.preprocessing import OneHotEncoder
-    X = []
-    for doc in corpus.docs:
-        doc_onehot = np.array()
+    X = np.zeros((corpus.M, corpus.V), dtype='int')
+    for doc_id, doc in enumerate(corpus.docs):
+        for word in doc:
+            X[doc_id][word] += 1
+
+    Y = np.array(corpus.context['usa'], dtype='int')
+
+    split_index = int(corpus.M * 0.9)
+
+    X_train = X[:split_index]
+    Y_train = Y[:split_index]
+
+    X_test = X[split_index:]
+    Y_test = Y[split_index:]
+
+    lr = LogisticRegression()
+    lr.fit(X_train, Y_train)
+
+    Y_predict = lr.predict(X_test)
+
+    precision = sum(map(lambda y1y2: int(y1y2[0] == y1y2[1]), zip(Y_test, Y_predict))) / float(len(X_test))
+
+    print 'USA or Not USA test Precision = ', precision
+
 
 def linear_classifier(lda_model, corpus):
 
-
-    split_index = int(corpus.M * 0.9)
     X = lda_model.theta
     Y = np.array(corpus.context['usa'], dtype='int')
+
+    split_index = int(corpus.M * 0.9)
 
     X_train = X[:split_index]
     Y_train = Y[:split_index]
@@ -57,6 +77,8 @@ def linear_classifier(lda_model, corpus):
 if __name__ == '__main__':
     corpus = Corpus()
     corpus.load_reuters(menu_path + 'reuters.json', menu_path + 'stopword.txt')
+
+    fit(corpus)
 
     model = LDA()
     model.load_model(filepath='save/reuters-21578/')
